cc9888226eb4be356556de658fad2d450063e0d7
Ben Bernardy <benbernardy@gmail.com>
2021-08-11 11:23:24 -0700

    Created gitignore and moved file structure

--- ProductOverview/schema.sql
+++ ProductOverview/schema.sql
@@ -10,16 +10,16 @@
 --
 -- ---
 
-DROP TABLE IF EXISTS `products`;
+-- DROP TABLE IF EXISTS "products";
 
-CREATE TABLE `products` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `name` VARCHAR(156) NULL DEFAULT NULL,
-  `slogan` VARCHAR(256) NULL DEFAULT NULL,
-  `descript` VARCHAR(320) NULL DEFAULT NULL,
-  `category` VARCHAR(60) NULL DEFAULT NULL,
-  `default_price` VARCHAR(60) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE "products" (
+  "id" bigserial NOT NULL,
+  "name" VARCHAR(156) NULL DEFAULT NULL,
+  "slogan" VARCHAR(256) NULL DEFAULT NULL,
+  "descript" VARCHAR(320) NULL DEFAULT NULL,
+  "category" VARCHAR(60) NULL DEFAULT NULL,
+  "default_price" VARCHAR(60) NULL DEFAULT NULL,
+  PRIMARY KEY ("id")
 );
 
 -- ---
@@ -27,29 +27,43 @@ CREATE TABLE `products` (
 --
 -- ---
 
-DROP TABLE IF EXISTS `styles`;
+-- DROP TABLE IF EXISTS "styles";
 
-CREATE TABLE `styles` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `id_products` INTEGER NULL DEFAULT NULL,
-  `sale_price` VARCHAR(60) NULL DEFAULT NULL,
-  `default_disp` VARCHAR(12) NULL DEFAULT 'false',
-  PRIMARY KEY (`id`)
+CREATE TABLE "styles" (
+  "id" bigserial NOT NULL,
+  "id_products" INTEGER NULL DEFAULT NULL,
+  "sale_price" VARCHAR(60) NULL DEFAULT NULL,
+  "default_disp" VARCHAR(12) NULL DEFAULT 'false',
+  PRIMARY KEY ("id")
 );
 
+-- ---
+-- Table 'cart'
+--
+-- ---
+
+-- DROP TABLE IF EXISTS "cart";
+
+CREATE TABLE "cart" (
+  "id" bigserial NOT NULL,
+  "id_products" INTEGER DEFAULT NULL,
+  "sale_price" VARCHAR(60) NULL DEFAULT NULL,
+  "default_disp" VARCHAR(12) NULL DEFAULT 'false',
+  PRIMARY KEY ("id")
+);
 -- ---
 -- Table 'photos'
 --
 -- ---
 
-DROP TABLE IF EXISTS `photos`;
+-- DROP TABLE IF EXISTS "photos";
 
-CREATE TABLE `photos` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `id_styles` INTEGER NULL DEFAULT NULL,
-  `thumb` VARCHAR(300) NULL DEFAULT NULL,
-  `url` VARCHAR(300) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE "photos" (
+  "id" bigserial NOT NULL,
+  "id_styles" INTEGER NULL DEFAULT NULL,
+  "thumb" VARCHAR(300) NULL DEFAULT NULL,
+  "url" VARCHAR(300) NULL DEFAULT NULL,
+  PRIMARY KEY ("id")
 );
 
 -- ---
@@ -57,14 +71,14 @@ CREATE TABLE `photos` (
 --
 -- ---
 
-DROP TABLE IF EXISTS `Feature`;
+DROP TABLE IF EXISTS "feature";
 
-CREATE TABLE `Feature` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `id_products` INTEGER NULL DEFAULT NULL,
-  `feature` VARCHAR(100) NULL DEFAULT NULL,
-  `value` VARCHAR(150) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE "feature" (
+  "id" bigserial NOT NULL,
+  "id_products" INTEGER NULL DEFAULT NULL,
+  "feature" VARCHAR(100) NULL DEFAULT NULL,
+  "value" VARCHAR(150) NULL DEFAULT NULL,
+  PRIMARY KEY ("id")
 );
 
 -- ---
@@ -72,46 +86,47 @@ CREATE TABLE `Feature` (
 --
 -- ---
 
-DROP TABLE IF EXISTS `inventory`;
+DROP TABLE IF EXISTS "inventory";
 
-CREATE TABLE `inventory` (
-  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
-  `id_styles` INTEGER NULL DEFAULT NULL,
-  `sku` INTEGER NULL DEFAULT NULL,
-  `quantity` INTEGER NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE "inventory" (
+  "id" bigserial NOT NULL,
+  "id_styles" INTEGER NULL DEFAULT NULL,
+  "sku" INTEGER NULL DEFAULT NULL,
+  "quantity" INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY ("id")
 );
 
 -- ---
 -- Foreign Keys
 -- ---
 
-ALTER TABLE `styles` ADD FOREIGN KEY (id_products) REFERENCES `products` (`id`);
-ALTER TABLE `photos` ADD FOREIGN KEY (id_styles) REFERENCES `styles` (`id`);
-ALTER TABLE `Feature` ADD FOREIGN KEY (id_products) REFERENCES `products` (`id`);
-ALTER TABLE `inventory` ADD FOREIGN KEY (id_styles) REFERENCES `styles` (`id`);
+ALTER TABLE "styles" ADD FOREIGN KEY (id_products) REFERENCES "products" ("id");
+ALTER TABLE "photos" ADD FOREIGN KEY (id_styles) REFERENCES "styles" ("id");
+ALTER TABLE "feature" ADD FOREIGN KEY (id_products) REFERENCES "products" ("id");
+ALTER TABLE "inventory" ADD FOREIGN KEY (id_styles) REFERENCES "styles" ("id");
+ALTER TABLE "cart" ADD FOREIGN KEY (id_products) REFERENCES "products" ("id");
 
 -- ---
 -- Table Properties
 -- ---
 
--- ALTER TABLE `products` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `styles` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `photos` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `Feature` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- ALTER TABLE `inventory` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE "products" ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE "styles" ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE "photos" ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE "Feature" ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE "inventory" ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- ---
 -- Test Data
 -- ---
 
--- INSERT INTO `products` (`id`,`name`,`slogan`,`descript`,`category`,`default_price`) VALUES
+-- INSERT INTO "products" ("id","name","slogan","descript","category","default_price") VALUES
 -- ('','','','','','');
--- INSERT INTO `styles` (`id`,`id_products`,`sale_price`,`default_disp`) VALUES
+-- INSERT INTO "styles" ("id","id_products","sale_price","default_disp") VALUES
 -- ('','','','');
--- INSERT INTO `photos` (`id`,`id_styles`,`thumb`,`url`) VALUES
+-- INSERT INTO "photos" ("id","id_styles","thumb","url") VALUES
 -- ('','','','');
--- INSERT INTO `Feature` (`id`,`id_products`,`feature`,`value`) VALUES
+-- INSERT INTO "Feature" ("id","id_products","feature","value") VALUES
 -- ('','','','');
--- INSERT INTO `inventory` (`id`,`id_styles`,`sku`,`quantity`) VALUES
+-- INSERT INTO "inventory" ("id","id_styles","sku","quantity") VALUES
 -- ('','','','');
