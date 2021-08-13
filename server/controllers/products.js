7d70b9d5f6f635a7b441d0bc5160fc680fe2d9d6
Ben Bernardy <benbernardy@gmail.com>
2021-08-13 12:02:00 -0700

    Saving work here while swithcing branches

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -13,17 +13,23 @@ module.exports = {
   product: (req, res) => {
     const { product_id } = req.params;
     dbUtils.getProduct(product_id)
-    .then(result => {
-      console.log(result, typeof result);
-      return result;
-    })
+      .then((result) => {
+        console.log(result, typeof result);
+        return result;
+      })
       .then((result) => res.set(200).send(result.rows[0]))
       .catch((e) => res.set(403).send(e.stack));
   },
-  // styles: (req, res) => {
-  //  // DB INTERACTION HERE;;
-  // },
-  // related: (req, res) => {
-  //   // DB INTERACTION HERE;;
-  // },
+  styles: (req, res) => {
+    const { product_id } = req.params;
+    dbUtils.getStyles(product_id)
+      .then((result) => res.set(200).send(result.rows))
+      .catch((e) => res.set(403).send(e.stack));
+  },
+  related: (req, res) => {
+    const { product_id } = req.params;
+    dbUtils.getRelated(product_id)
+      .then((result) => res.set(200).send(result.rows))
+      .catch((e) => res.set(403).send(e.stack));
+  },
 };
