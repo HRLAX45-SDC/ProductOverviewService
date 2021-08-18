85cca5eeda5f4e8f9b9f720e7cbe1cebc0252767
Ben Bernardy <benbernardy@gmail.com>
2021-08-18 11:40:55 -0700

    CHANGE: Now with ENV variable access

--- db/index.js
+++ db/index.js
@@ -1,12 +1,17 @@
 const pg = require('pg');
+const DB_PASS = process.env.DB_PASS;
+const USER = process.env.USER;
+const DB = process.env.DB
+const DB_PORT = process.env.DB_PORT;
+const HOST = process.env.HOST;
 
 const pool = new pg.Pool({
   // Connection info for pool. Note to not call pool.end() till program end.
-  user: 'postgres',
-  host: '127.0.0.1',
-  database: 'prod_over',
-  password: '123qwe!@#QWE',
-  port: 5432,
+  user: USER,
+  host: HOST,
+  database: DB,
+  password: DB_PASS,
+  port: DB_PORT,
 });
 
 pool.query('SELECT NOW()', (err, res) => {
